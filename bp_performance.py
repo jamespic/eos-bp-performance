13c3e2e1197aa0ce4ece9804cbcadc29ddc69ac3
James Pickering <james_pic@hotmail.com>
2018-09-02 20:58:28 +0100

    Add caching

--- bp_performance.py
+++ bp_performance.py
@@ -14,6 +14,7 @@ from concurrent.futures import ThreadPoolExecutor
 from cheroot.wsgi import Server, PathInfoDispatcher
 from urllib.request import urlopen
 from werkzeug.wsgi import pop_path_info
+from werkzeug.wrappers import Request, Response
 
 
 class BPPerformance:
@@ -200,6 +201,62 @@ classifiers = {
 }
 
 
+def cache_middleware(expiry_seconds):
+    expiry = datetime.timedelta(seconds=expiry_seconds)
+    cache = {}
+    def wrapper(f):
+        def wrapped(environ, start_response):
+            req = Request(environ)
+            path = req.path
+            cached = cache.get(path)
+            if cached:
+                updated_time, content, status, headers = cached
+                if updated_time + expiry > datetime.datetime.now():
+                    # Cached version still valid
+                    browser_cache_valid = False
+                    etag = str(hash(content))
+                    if req.if_none_match.contains(etag):
+                        browser_cache_valid = True
+                    if req.if_modified_since:
+                        if updated_time < req.if_modified_since:
+                            browser_cache_valid = True
+                    if browser_cache_valid:
+                        start_response('304 Not Modified', [])
+                        return []
+                    else:
+                        response = Response(content, status, headers)
+                        response.add_etag(etag)
+                        response.date = datetime.datetime.now()
+                        return response(environ, start_response)
+            # Cached version not valid
+            sent_status = None
+            sent_headers = []
+            def inner_start_response(status, headers, exc=None):
+                nonlocal sent_status, sent_headers
+                if exc:
+                    start_response(status, headers, exc)
+                else:
+                    sent_status = status
+                    sent_headers = list(headers)
+            response_iter = f(environ, inner_start_response)
+            if sent_status:
+                # Success! Cache and send response
+                content = tuple(response_iter)
+                cache[path] = (datetime.datetime.now(), content, sent_status, sent_headers)
+                response = Response(content, sent_status, sent_headers)
+                response.set_etag(str(hash(content)))
+                response.date = datetime.datetime.now()
+                return response(environ, start_response)
+            else:
+                # Don't cache the exception handler, just propagate
+                return response_iter
+        return wrapped
+    return wrapper
+
+def hello(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain; charset=utf-8')])
+    return [datetime.datetime.now().isoformat().encode('utf-8')]
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Run a web server with stats about EOS block producer performance")
@@ -213,10 +270,13 @@ if __name__ == '__main__':
     thread = threading.Thread(target=bp_perf.watch)
     thread.start()
 
-    app = PathInfoDispatcher({
-        '/': index(bp_perf),
-        '/chart': chart_renderer(bp_perf)
-    })
+    app = cache_middleware(60)(
+        PathInfoDispatcher({
+            '/': index(bp_perf),
+            '/chart': chart_renderer(bp_perf),
+            '/hello': hello
+        })
+    )
 
     httpd = Server((args.host, args.port), app)
 
